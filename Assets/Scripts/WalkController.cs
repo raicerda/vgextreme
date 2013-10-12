ed4d8c355d056e393d08f5d3f0ef2aa77b31ff29
Raimundo Cerda <rai.cerda@gmail.com>
2013-10-12 15:31:28 -0400

    grapple functions

--- Assets/Scripts/WalkController.cs
+++ Assets/Scripts/WalkController.cs
@@ -5,11 +5,14 @@ public class WalkController : MonoBehaviour {
 	public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
+	public bool isGrappable = false;
     private Vector3 moveDirection = Vector3.zero;
+	
 	
     void Update() {
 		
         CharacterController controller = GetComponent<CharacterController>();
+		
         if (controller.isGrounded) {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
@@ -18,7 +21,32 @@ public class WalkController : MonoBehaviour {
                 moveDirection.y = jumpSpeed;
             
         }
+		moveDirection.Set (moveDirection.x,moveDirection.y,0);
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+		if(Input.GetKey(KeyCode.Z)){
+			if(isGrappable){
+				rigidbody.velocity = Vector3.zero;
+				//TODO Sprite Changes
+			}			
+		}
     }
+	
+	
+	
+	
+	// Check if the character can grapple to the wall
+	void OnCollisionEnter(Collision target){
+		if (target.transform.tag == "dirtWall") {
+			isGrappable = true;
+		}
+	}
+	
+	void OnCollisionExit(Collision target){
+		if (target.transform.tag == "dirtWall"){
+			isGrappable = false;
+		}
+	}
+	
 }
+
